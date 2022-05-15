bfe812a70ee5e608dc2edf734848a710c40d9672
Idan Pazi <idan.kp@gmail.com>
2022-05-15 11:32:46 +0300

    add matplotlib.pyplot to user as plt

--- calcpy/user.py
+++ calcpy/user.py
@@ -3,6 +3,10 @@ try:
     import numpy as np
 except ModuleNotFoundError:
     pass
+try:
+    import matplotlib.pyplot as plt
+except ModuleNotFoundError:
+    pass
 import sympy
 from sympy.parsing.latex import parse_latex
 from sympy import *
