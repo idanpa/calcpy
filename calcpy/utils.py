b0584eb617229680ca1b29cfe8ac661140c2d96a
Idan Pazi <idan.kp@gmail.com>
2023-06-07 08:35:53 +0300

    copy - use pyperclip if possible

--- calcpy/utils.py
+++ calcpy/utils.py
@@ -1,10 +1,15 @@
-from tkinter import Tk
 
 def copy(object):
     '''Copy object representation 'repr(object)' to clipboard'''
-    r = Tk()
-    r.withdraw()
-    r.clipboard_clear()
-    r.clipboard_append(repr(object))
-    r.update()
-    r.destroy()
+    try:
+        import pyperclip
+        pyperclip.copy(repr(object))
+    except ModuleNotFoundError:
+        # this is a bit slower, and when pasting too fast, clash with ipython's access to clipboard
+        from tkinter import Tk
+        r = Tk()
+        r.withdraw()
+        r.clipboard_clear()
+        r.clipboard_append(repr(object))
+        r.update()
+        r.destroy()
