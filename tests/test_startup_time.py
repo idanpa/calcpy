6cf08aa293ca7a3d8ab5c6f54b13e61597c79e1f
Idan Pazi <idan.kp@gmail.com>
2023-06-17 23:45:20 +0300

    add tests

---
+++ tests/test_startup_time.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import subprocess
+import time
+
+def test_startup_time(n=10):
+    # warmup:
+    subprocess.check_call(['calcpy', '-c', '4+4'], stdout=None, stdin=subprocess.PIPE)
+
+    elapsed = []
+    for i in range(n):
+        start_time = time.time()
+        subprocess.check_call(['calcpy', '-c', '4+4'], stdout=None, stdin=subprocess.PIPE)
+        elapsed.append(time.time() - start_time)
+    print(elapsed)
+    print(f'max {max(elapsed)}')
+    print(f'min {min(elapsed)}')
+    print(f'avg {sum(elapsed)/n}')
+
+if __name__ == "__main__":
+    test_startup_time()
