d64514f337d98dbfca4ad4fc6d7bd849d329b9eb
Idan Pazi <idan.kp@gmail.com>
2023-06-17 10:10:23 +0300

    setup - bug fix - IPython is needed for setup itself
    Although the reccomended way is to use pyproject.toml,
    would be done in the future.
    
    Also remove calcpy import.

--- setup.py
+++ setup.py
@@ -8,16 +8,17 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
+CALCPY_PROFILE_NAME = 'calcpy'
+
 class post_setup(install):
     def run(self):
         install.run(self)
         import IPython
-        import calcpy
         try:
-            calcpy_profile_path = IPython.paths.locate_profile(calcpy.CALCPY_PROFILE_NAME)
+            calcpy_profile_path = IPython.paths.locate_profile(CALCPY_PROFILE_NAME)
         except OSError:
-            IPython.core.profiledir.ProfileDir.create_profile_dir_by_name(IPython.paths.get_ipython_dir(), calcpy.CALCPY_PROFILE_NAME)
-            calcpy_profile_path = IPython.paths.locate_profile(calcpy.CALCPY_PROFILE_NAME)
+            IPython.core.profiledir.ProfileDir.create_profile_dir_by_name(IPython.paths.get_ipython_dir(), CALCPY_PROFILE_NAME)
+            calcpy_profile_path = IPython.paths.locate_profile(CALCPY_PROFILE_NAME)
         try:
             with open(os.path.join(calcpy_profile_path, 'user_startup.py'), 'x') as f:
                 f.write('# CalcPy user startup:')
@@ -52,7 +53,7 @@ setuptools.setup(
         ## for parse_latex(): antlr4-python3-runtime (pip), antlr-python-runtime (conda)
     ],
     use_scm_version={'fallback_version':'0.0.0'},
-    setup_requires=['setuptools_scm'],
+    setup_requires=['setuptools_scm', 'ipython'], #TODO: move to pyproject.toml
     license='MIT License',
     zip_safe=False,
     keywords=['calculator',],
