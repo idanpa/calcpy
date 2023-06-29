1656f2c7503d36965c8ac9c36ff36d3c84011732
Idan Pazi <idan.kp@gmail.com>
2023-06-29 08:47:15 +0300

    tests - fix tests transformers

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -4,27 +4,24 @@ def test_autodate(ip):
     dt = ip.run_cell('d"today"-d"yesterday"').result
     assert dt.days == 1 or 24*60*60-1 <= dt.seconds <= 24*60*60
 
-def implicit_multiply(ip):
+def test_implicit_multiply(ip):
     ip.run_cell('foo=1')
     assert ip.run_cell('2foo').result == 2
 
-def auto_solve(ip):
-    assert set(ip.run_cell('x**2=1').result) == set([-1,1])
-
-def caret_power(ip):
+def test_caret_power(ip):
     ip.calcpy.caret_power = True
     assert ip.run_cell('3^3').result == 27
 
-def auto_lambda(ip):
+def test_auto_lambda(ip):
     ip.run_cell('f(x,y):=x+y')
     assert ip.run_cell('f(1,2)').result == 3
 
-def auto_matrix(ip):
+def test_auto_matrix(ip):
     assert ip.run_cell('((1,0),(0,1)).det()').result == 1
 
-def parse_latex(ip):
+def test_parse_latex(ip):
     assert ip.run_cell('$\\frac{1}{2}$.evalf()').result == 0.5
 
-def auto_symbols(ip):
+def test_auto_symbols(ip):
     assert ip.run_cell('x+y_1+z2').result == symbols('x')+symbols('y_1')+symbols('z2')
 
