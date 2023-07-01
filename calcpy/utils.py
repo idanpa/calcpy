b766beb34c042e85a4e80d31549c5adb0202ff84
Idan Pazi <idan.kp@gmail.com>
2023-07-01 07:23:43 +0300

    copy - don't repr strings
    e.g. copy(latex(_))

--- calcpy/utils.py
+++ calcpy/utils.py
@@ -1,15 +1,17 @@
 
-def copy(object):
-    '''Copy object representation 'repr(object)' to clipboard'''
+def copy(obj):
+    '''Copy obj representation 'repr(obj)' to clipboard'''
+    if not isinstance(obj, str):
+        obj = repr(obj)
     try:
         import pyperclip
-        pyperclip.copy(repr(object))
+        pyperclip.copy(obj)
     except ModuleNotFoundError:
         # this is a bit slower, and when pasting too fast, clash with ipython's access to clipboard
         from tkinter import Tk
         r = Tk()
         r.withdraw()
         r.clipboard_clear()
-        r.clipboard_append(repr(object))
+        r.clipboard_append(obj)
         r.update()
         r.destroy()
